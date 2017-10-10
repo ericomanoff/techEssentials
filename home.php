685555dfaa932fe597c7ae47051820ba5c359f86
ericomanoff <eric.omanoff@gmail.com>
2017-10-09 20:16:43 -0600

    initial commit

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The home template file.
+ *
+ * @package Astrid
+ */
+
+get_header(); ?>
+
+
+	<div id="primary" class="content-area  home<?php echo esc_attr(astrid_blog_layout()); ?>">
+		<main id="main" class="site-main home-main" role="main">
+
+			<h1>home</h1>
+
+		<?php if ( have_posts() ) : ?>
+
+			<div class="posts-layout">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+				<?php endwhile; ?>
+			</div>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+	if ( astrid_blog_layout() == 'list' ) :
+		get_sidebar();
+	endif;
+?>
+<?php get_footer(); ?>
---
+++ page-templates/front-page.php
@@ -0,0 +1,185 @@
+<?php
+
+/*
+
+Template Name: Front Page
+
+*/
+get_header();
+?>
+
+
+</div>
+	<div id="primary" class="content-area fullwidth">
+		<main id="main" class="site-main" role="main">
+
+    <section id="services" class="text-center home-section">
+      <div class="container">
+        <div class="row">
+           <h1 class="text-upper bottom40">Services</h1>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <i class="fa fa-phone" aria-hidden="true"></i>
+            <h3>Business Phone Systems</h3>
+            <p>Affordable, feauture rich phone systems that help your business fly.</p>
+          </div>
+          <div class="col-sm-4">
+            <i class="fa fa-server" aria-hidden="true"></i>
+            <h3>Hosted Phone Service</h3>
+            <p>We'll handle your host your phone system so you can handle your business.</p>
+          </div>
+          <div class="col-sm-4">
+            <i class="fa fa-mobile" aria-hidden="true"></i>
+            <h3>Mobile Extension</h3>
+            <p>Seamlessly receive calls on your mobile while away from the office.</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-4">
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+            <h3>Voice-to-Email</h3>
+            <p>Stay organized with ultra convenient Voice-to-Email service.</p>
+          </div>
+          <div class="col-sm-4">
+            <i class="fa fa-database" aria-hidden="true"></i>
+            <h3>Local IT Provider</h3>
+            <p>Get up and running fast with a local partner and provider of IT solutions.</p>
+          </div>
+          <div class="col-sm-4">
+            <i class="fa fa-cloud" aria-hidden="true"></i>
+            <h3>Cloud Services</h3>
+            <p>Leverage our Cloud Hosting Services to find a custom and afforadable solution.</p>
+          </div>
+      </div>
+    </section>
+
+
+    <section id="products" class="text-center home-section">
+      <div class="container">
+        <div class="row">
+           <h1 class="text-upper bottom40 text-white">Products</h1>
+           <p>Check out some of our most popular products.</p>
+        </div>
+      </div>
+    </section>
+
+    <section id="products-2" class="text-center home-section-no-padding bg-white">
+      <div class="project">
+          <div class="project-thumb">
+            <img width="500" height="310" src="<?php echo get_template_directory_uri(); ?>/images/prod-1.png" class="attachment-astrid-project-thumb size-astrid-project-thumb wp-post-image" alt="" />
+            <div class="project-content">
+              <h3 class="project-title"><a href="#">Product 1</a></h3>
+            </div>
+          </div>
+      </div>
+      <div class="project">
+          <div class="project-thumb">
+            <img width="500" height="310" src="<?php echo get_template_directory_uri(); ?>/images/prod-2.png" class="attachment-astrid-project-thumb size-astrid-project-thumb wp-post-image" alt="" />
+            <div class="project-content">
+              <h3 class="project-title"><a href="#">Product 2</a></h3>
+            </div>
+          </div>
+      </div>
+      <div class="project">
+          <div class="project-thumb">
+            <img width="500" height="310" src="<?php echo get_template_directory_uri(); ?>/images/prod-4.jpg" class="attachment-astrid-project-thumb size-astrid-project-thumb wp-post-image" alt="" />
+            <div class="project-content">
+              <h3 class="project-title"><a href="#">Product 3</a></h3>
+            </div>
+          </div>
+      </div>
+      <div class="project">
+          <div class="project-thumb">
+            <img width="500" height="310" src="<?php echo get_template_directory_uri(); ?>/images/prod-3.jpg" class="attachment-astrid-project-thumb size-astrid-project-thumb wp-post-image" alt="" />
+            <div class="project-content">
+              <h3 class="project-title"><a href="#">Product 4</a></h3>
+            </div>
+          </div>
+      </div>
+
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+<div class="multicolumn-row clearfix">
+
+    <section  style="background-color:#202529;padding-top:100px;padding-bottom:100px;background-image:url();" id="text-3" class="widget clearfix at-2-col widget_text"><div class="atblock container"><h2 class="widget-title" style="color:#ffffff;"><span class="title-decoration"></span>ABOUT US</h2>      <div class="textwidget">
+
+		<p>Technology Essentials is an Unified Communications and Cloud consulting company located in Fort Collins, CO and serves companies in Northern Colorado by providing them with the essential technology and IT applications they will need to do business – this includes email, file backup &amp; sharing, voice &amp; internet service, business phone systems, unified communications, cloud servers, and IT hardware &amp; software.  Our main goal is to give our customers the freedom and ability to not worry about their technology – so they can concentrate on their business.</p>
+		<p>Our hope is to be your partner of choice with respect to your company’s basic technology needs.</p>
+
+	   </div>
+    </div>
+  </section>
+
+    <section style="" id="atframework_skills-3" class="widget clearfix at-2-col atframework_skills_widget"><div class="atblock container" style="color: inherit;">
+
+
+                <div class="skill" style="color: inherit;">
+        <div class="name" style="color: inherit;">Phone Systems</div>
+              <div class="progress-bar" style="color: inherit;">
+          <div class="progress-animate" style="width: 90%; color: inherit;"></div>
+        </div>
+      </div>
+
+                <div class="skill" style="color: inherit;">
+        <div class="name" style="color: inherit;">Support</div>
+              <div class="progress-bar" style="color: inherit;">
+          <div class="progress-animate" style="width: 92%; color: inherit;"></div>
+        </div>
+      </div>
+
+                <div class="skill" style="color: inherit;">
+        <div class="name" style="color: inherit;">Custom Solutions</div>
+              <div class="progress-bar" style="color: inherit;">
+          <div class="progress-animate" style="width: 90%; color: inherit;"></div>
+        </div>
+      </div>
+
+                <div class="skill" style="color: inherit;">
+        <div class="name" style="color: inherit;">Security</div>
+              <div class="progress-bar" style="color: inherit;">
+          <div class="progress-animate" style="width: 92%; color: inherit;"></div>
+        </div>
+      </div>
+
+                <div class="skill" style="color: inherit;">
+        <div class="name" style="color: inherit;">Mobile</div>
+              <div class="progress-bar" style="color: inherit;">
+          <div class="progress-animate" style="width: 90%; color: inherit;"></div>
+        </div>
+      </div>
+
+
+  </div>
+  </section>
+
+  <section id="contact" class="text-center home-section">
+
+    <!-- <video autoplay loop id="video-background" muted plays-inline>
+      <source src="< ?php echo get_template_directory_uri(); ?>/images/video.mp4" type="video/mp4">
+    </video> -->
+
+    <div class="container">
+      <a href="/contact" class="button quote">Get a Quote</a>
+    </div>
+
+  </section>
+
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+  get_footer();
+?>
